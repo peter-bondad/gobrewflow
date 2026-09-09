6f738e7c8517cae74a758d781011b28e086cb78d
Peter Bondad <bondadpetermaironne@gmail.com>
2026-09-09 12:47:43 +0800

    refactor(products): transition error constants to error types

--- internal/services/products/errors.go
+++ internal/services/products/errors.go
@@ -1,5 +1,11 @@
 package products
 
-const (
-	ErrProductNotFound = "product not found"
+import "errors"
+
+var (
+	ProductIdIsRequired    = errors.New("product id is required")
+	ProductInputIsRequired = errors.New("product input is required")
+	ProductNameIsRequired  = errors.New("product name is required")
+	ProductSKUIsRequired   = errors.New("product sku is required")
+	ErrProductNotFound     = errors.New("product not found")
 )
