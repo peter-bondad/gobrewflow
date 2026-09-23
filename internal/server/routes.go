e54d70287b1f7931b9bc3db45d815c0f08610242
Peter Bondad <bondadpetermaironne@gmail.com>
2026-09-23 20:58:12 +0800

    refactor(api): change stock adjustment route method to PATCH

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -89,7 +89,7 @@ func (s *Server) protectedRoutes(
 
 	// Inventory
 	inventoryAPI.GET("/:productId", inventoryHandler.GetInventoryByProductID)
-	inventoryAPI.POST("/:productId/adjust", inventoryHandler.AdjustStock)
+	inventoryAPI.PATCH("/:productId/adjust", inventoryHandler.AdjustStock)
 
 	// Orders
 	ordersAPI.POST("/", ordersHandler.CreateOrder)
